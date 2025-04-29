88c2d5eeef4165bf795dec08048e6811cc612c74
Triet Le <trietle847@gmail.com>
2025-04-29 13:45:31 +0700

    add template for prompt

---
+++ backend/app/templates/prompt.template.js
@@ -0,0 +1,3 @@
+module.exports = {
+  vocabularyPromptTemplate: `Generate 20 different English vocabulary on {{topic}} topics including \"word\", \"phonetics\", \"meanings\", \"examples\", \"synonyms\", \"antonyms\". The \"meanings\" field must be an object with keys: \"english\" and \"vietnamese\", represented as JSON`,
+};
