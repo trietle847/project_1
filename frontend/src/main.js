011b2f79b37df5d40d478c2c6d02b36af9d3f45e
Triet Le <trietle847@gmail.com>
2025-05-02 13:48:55 +0700

    update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
-
+import drag from './helpers/drag.helper';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-createApp(App).use(router).mount("#app");
+// createApp(App).use(router).mount("#app");
+
+const app = createApp(App);
+app.use(router);
+app.directive("drag", drag);
+
+app.mount("#app");
