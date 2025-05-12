169ac5d73dbafd214374358b3d2533c5b96352ad
Triet Le <trietle847@gmail.com>
2025-05-12 08:59:52 +0700

    update

--- frontend/src/services/generateByAI.service.js
+++ frontend/src/services/generateByAI.service.js
@@ -4,5 +4,8 @@ export default {
   generateVocabulary(topic) {
     return createAPI.post("/chatgpt/generate-vocabulary", topic);
   },
+  updateVocabulary() {
+    return createAPI.get("/chatgpt/update-vocabulary");
+  }
 
 };
