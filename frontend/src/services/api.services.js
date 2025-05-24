0b7ae90f9a11f70d50f93c60ddb4487eab632029
trietle847 <trietle847@gmail.com>
2025-05-24 11:01:40 +0700

    update

--- frontend/src/services/api.services.js
+++ frontend/src/services/api.services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const createAPI = axios.create({
-  baseURL: "http://localhost:3000", // Thay đổi tùy theo server
+  baseURL: import.meta.env.VITE_API_URL, // Thay đổi tùy theo server "http://localhost:3000"
   headers: {
     "Content-Type": "application/json",
   },
